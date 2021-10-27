01d36805a759e6b14a7ce144e94d70305c2148ee
Tejas Borde <tejas.borde@kpit.com>
2021-10-27 22:06:41 +0530

    commit

--- src/components/QuestionComponents/Quizs.js
+++ src/components/QuestionComponents/Quizs.js
@@ -1,15 +1,44 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
+import React, { useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Quizzs } from "../../App";
 import { BiArrowBack } from "react-icons/bi";
 
-const Quizs = () => {
+const Quizs = (props) => {
+  const QuizList = useContext(Quizzs);
+  const { subject } = useParams();
+
   return (
     <div className="quizs">
       <div className="container">
         <Link className="back-btn" to="/quizzes/subjects">
           <BiArrowBack />
         </Link>
+        {QuizList.map((quiz) => {
+          if (quiz.subject === subject) {
+            return (
+              <div className="container quiz-list-component">
+                <div className="row">
+                  <div className="col-6">
+                    <div className="row">Name : {quiz.name}</div>
+                    <div className="row">Subject : {quiz.subject}</div>
+                    <div className="row">
+                      Number of Questions : {quiz.no_of_questions}
+                    </div>
+                  </div>
+                  <div className="col-6 take-test">
+                    <div className="row">
+                      <Link to={"/quiz/" + quiz._id}>
+                        <button className="quiz-btn">Take a Quiz</button>
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          } else {
+            return "";
+          }
+        })}
       </div>
     </div>
   );
