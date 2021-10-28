46a75462d9694ade491d7ce46057cc7694d4543e
Tejas Borde <tejas.borde@kpit.com>
2021-10-28 20:57:17 +0530

    one more quiz added

--- src/Data/AllQuizs.js
+++ src/Data/AllQuizs.js
@@ -762,4 +762,341 @@ export const AllQuizList = [
       },
     ],
   },
+  {
+    _id: 3,
+    chapter_no: 3,
+    name: "Price Determination in Different Market",
+    subject: "Economics",
+    topic: "",
+    Questions: [
+      {
+        que_no: 1,
+        correct_ans: "A",
+      },
+      {
+        que_no: 2,
+        correct_ans: "A",
+      },
+      {
+        que_no: 3,
+        correct_ans: "D",
+      },
+      {
+        que_no: 4,
+        correct_ans: "B",
+      },
+      {
+        que_no: 5,
+        correct_ans: "B",
+      },
+      {
+        que_no: 6,
+        correct_ans: "B",
+      },
+      {
+        que_no: 7,
+        correct_ans: "C",
+      },
+      {
+        que_no: 8,
+        correct_ans: "A",
+      },
+      {
+        que_no: 9,
+        correct_ans: "D",
+      },
+      {
+        que_no: 10,
+        correct_ans: "C",
+      },
+      {
+        que_no: 11,
+        correct_ans: "C",
+      },
+      {
+        que_no: 12,
+        correct_ans: "A",
+      },
+      {
+        que_no: 13,
+        correct_ans: "B",
+      },
+      {
+        que_no: 14,
+        correct_ans: "C",
+      },
+      {
+        que_no: 15,
+        correct_ans: "A",
+      },
+      {
+        que_no: 16,
+        correct_ans: "B",
+      },
+      {
+        que_no: 17,
+        correct_ans: "D",
+      },
+      {
+        que_no: 18,
+        correct_ans: "D",
+      },
+      {
+        que_no: 19,
+        correct_ans: "A",
+      },
+      {
+        que_no: 20,
+        correct_ans: "B",
+      },
+      {
+        que_no: 21,
+        correct_ans: "A",
+      },
+      {
+        que_no: 22,
+        correct_ans: "D",
+      },
+      {
+        que_no: 23,
+        correct_ans: "A",
+      },
+      {
+        que_no: 24,
+        correct_ans: "C",
+      },
+      {
+        que_no: 25,
+        correct_ans: "C",
+      },
+      {
+        que_no: 26,
+        correct_ans: "A",
+      },
+      {
+        que_no: 27,
+        correct_ans: "C",
+      },
+      {
+        que_no: 28,
+        correct_ans: "C",
+      },
+      {
+        que_no: 29,
+        correct_ans: "C",
+      },
+      {
+        que_no: 30,
+        correct_ans: "A",
+      },
+      {
+        que_no: 31,
+        correct_ans: "C",
+      },
+      {
+        que_no: 32,
+        correct_ans: "D",
+      },
+      {
+        que_no: 33,
+        correct_ans: "D",
+      },
+      {
+        que_no: 34,
+        correct_ans: "C",
+      },
+      {
+        que_no: 35,
+        correct_ans: "B",
+      },
+      {
+        que_no: 36,
+        correct_ans: "C",
+      },
+      {
+        que_no: 37,
+        correct_ans: "D",
+      },
+      {
+        que_no: 38,
+        correct_ans: "A",
+      },
+      {
+        que_no: 39,
+        correct_ans: "D",
+      },
+      {
+        que_no: 40,
+        correct_ans: "D",
+      },
+      {
+        que_no: 41,
+        correct_ans: "A",
+      },
+      {
+        que_no: 42,
+        correct_ans: "B",
+      },
+      {
+        que_no: 43,
+        correct_ans: "A",
+      },
+      {
+        que_no: 44,
+        correct_ans: "D",
+      },
+      {
+        que_no: 45,
+        correct_ans: "D",
+      },
+      {
+        que_no: 46,
+        correct_ans: "C",
+      },
+      {
+        que_no: 47,
+        correct_ans: "B",
+      },
+      {
+        que_no: 48,
+        correct_ans: "C",
+      },
+      {
+        que_no: 49,
+        correct_ans: "A",
+      },
+      {
+        que_no: 50,
+        correct_ans: "A",
+      },
+      {
+        que_no: 51,
+        correct_ans: "A",
+      },
+      {
+        que_no: 52,
+        correct_ans: "D",
+      },
+      {
+        que_no: 53,
+        correct_ans: "C",
+      },
+      {
+        que_no: 54,
+        correct_ans: "D",
+      },
+      {
+        que_no: 55,
+        correct_ans: "D",
+      },
+      {
+        que_no: 56,
+        correct_ans: "B",
+      },
+      {
+        que_no: 57,
+        correct_ans: "B",
+      },
+      {
+        que_no: 58,
+        correct_ans: "B",
+      },
+      {
+        que_no: 59,
+        correct_ans: "D",
+      },
+      {
+        que_no: 60,
+        correct_ans: "B",
+      },
+      {
+        que_no: 61,
+        correct_ans: "C",
+      },
+      {
+        que_no: 62,
+        correct_ans: "D",
+      },
+      {
+        que_no: 63,
+        correct_ans: "A",
+      },
+      {
+        que_no: 64,
+        correct_ans: "B",
+      },
+      {
+        que_no: 65,
+        correct_ans: "B",
+      },
+      {
+        que_no: 66,
+        correct_ans: "D",
+      },
+      {
+        que_no: 67,
+        correct_ans: "D",
+      },
+      {
+        que_no: 68,
+        correct_ans: "B",
+      },
+      {
+        que_no: 69,
+        correct_ans: "B",
+      },
+      {
+        que_no: 70,
+        correct_ans: "D",
+      },
+      {
+        que_no: 71,
+        correct_ans: "C",
+      },
+      {
+        que_no: 72,
+        correct_ans: "D",
+      },
+      {
+        que_no: 73,
+        correct_ans: "C",
+      },
+      {
+        que_no: 74,
+        correct_ans: "B",
+      },
+      {
+        que_no: 75,
+        correct_ans: "B",
+      },
+      {
+        que_no: 76,
+        correct_ans: "A",
+      },
+      {
+        que_no: 77,
+        correct_ans: "D",
+      },
+      {
+        que_no: 78,
+        correct_ans: "D",
+      },
+      {
+        que_no: 79,
+        correct_ans: "C",
+      },
+      {
+        que_no: 80,
+        correct_ans: "D",
+      },
+      {
+        que_no: 81,
+        correct_ans: "D",
+      },
+      {
+        que_no: 82,
+        correct_ans: "C",
+      },
+    ],
+  },
 ];
